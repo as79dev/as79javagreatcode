c336aab95b2edbac451a4fa1e480885cef4479bc
as79 <as79dev@gmail.com>
2021-09-23 22:02:08 +0200

    Feature1commit1

---
+++ src/main/java/As79String.java
@@ -0,0 +1,22 @@
+import static java.lang.System.*;
+
+public class As79String {
+    /**
+     * Test
+     * Kommentar
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Done");
+    }
+
+    // Started working on branch string
+    protected Integer countLength(@org.jetbrains.annotations.NotNull String s) {
+        Integer i = s.length;
+        System.out.println(s.length);
+        return i;
+    }
+
+
+}
